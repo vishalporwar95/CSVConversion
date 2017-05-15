3ac0d74df282d0f6cbfd42d335754ef90434c2d3
vishalporwar95 <vishal.jain@accoliteindia.com>
2017-05-15 15:22:48 +0530

    This contains form and its contents that are used in conversion of csv data to the required format

---
+++ Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿using System;
+
+namespace WindowsFormsApplication4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.UploadButton = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(24, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(362, 300);
+            this.panel1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(239, 15);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(99, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(99, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(119, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 20);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.linkLabel1);
+            this.panel2.Controls.Add(this.UploadButton);
+            this.panel2.Location = new System.Drawing.Point(27, 345);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(359, 72);
+            this.panel2.TabIndex = 1;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(233, 22);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(73, 13);
+            this.linkLabel1.TabIndex = 1;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Add more files";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // UploadButton
+            // 
+            this.UploadButton.Location = new System.Drawing.Point(33, 13);
+            this.UploadButton.Name = "UploadButton";
+            this.UploadButton.Size = new System.Drawing.Size(75, 30);
+            this.UploadButton.TabIndex = 0;
+            this.UploadButton.Text = "Upload";
+            this.UploadButton.UseVisualStyleBackColor = true;
+            this.UploadButton.Click += new System.EventHandler(this.Upload_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(451, 434);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button UploadButton;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        int  noofbuttons=1;
+
+        private List<TextBox> textboxes1 = new List<TextBox>();
+       private List<TextBox> textboxes2 = new List<TextBox>();
+
+        int text1x = 3, gridy=40,text2x=119,text3x= 239;
+        List<String> selectedfiles=new List<String>();
+        public Form1()
+        {
+            
+            InitializeComponent();
+            textboxes1.Add(this.textBox1);
+            textboxes2.Add(this.textBox2);
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+
+        private void Upload_Click(object sender, EventArgs e)
+        {
+            foreach (string file in selectedfiles)
+            {
+                string path = Path.GetFullPath(file);
+                string outputpath = Path.ChangeExtension(path, ".xls");
+
+                string[] CSVDump = File.ReadAllLines(path);
+                List<List<string>> records = CSVDump.Select(x => x.Split(';').ToList()).ToList();
+                for (int recordno = 0; recordno < records.Count; recordno++)
+                {
+                    records[recordno].RemoveAt(20);
+                    records[recordno].RemoveAt(18);
+                    records[recordno].RemoveAt(17);
+                    records[recordno].RemoveAt(16);
+                    records[recordno].RemoveAt(15);
+                    records[recordno].RemoveAt(14);
+                    records[recordno].RemoveAt(13);
+                    records[recordno].RemoveAt(12);
+                    records[recordno].RemoveAt(11);
+                    records[recordno].RemoveAt(10);
+                    records[recordno].RemoveAt(9);
+                    records[recordno].RemoveAt(8);
+                    records[recordno].RemoveAt(7);
+                    records[recordno].RemoveAt(6);
+                    records[recordno].RemoveAt(4);
+                    records[recordno].Insert(1, recordno == 0 ? "location" : "delhi");
+                    records[recordno].Insert(7, recordno == 0 ? "CTRLCOMPANY" : "delhi");
+                    records[recordno].Insert(8, recordno == 0 ? "EMPLOCAL" : "delhi");
+
+                }
+                File.WriteAllLines(outputpath, records.Select(x => string.Join(";", x)));
+            }
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //if (sender.Name == "hi")
+            //{ }
+            // 
+            Button button = (Button)sender;
+            int index = button.Name.IndexOf("button");
+            int selectedbuttonno = Convert.ToInt32(button.Name.Remove(index, 6));
+            
+            OpenFileDialog openfiledialog = new OpenFileDialog();
+            openfiledialog.Filter = "csv files (*.csv)|*.csv";
+            
+            if (openfiledialog.ShowDialog() == DialogResult.OK)
+            {   selectedfiles.Add(openfiledialog.FileName);
+                
+                textboxes1[selectedbuttonno-1].Text = openfiledialog.FileName;
+                textboxes2[selectedbuttonno-1].Text = "Selected";
+
+              //  i++;
+            }
+            else
+            {
+                textboxes1[selectedbuttonno - 1].Text = "";
+
+                textboxes2[selectedbuttonno-1].Text = "Not Selected";
+            }
+          }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            noofbuttons++;
+            TextBox newtextbox = new TextBox();
+            newtextbox.Location = new System.Drawing.Point(text1x, gridy);
+            //y += 30;
+            newtextbox.Name = "newtextbox";
+            newtextbox.Size = new System.Drawing.Size(99, 20);
+            newtextbox.TabIndex = 0;
+            textboxes1.Add(newtextbox);
+            panel1.Controls.Add(newtextbox);
+            TextBox newtextbox2 = new TextBox();
+            newtextbox2.Location = new System.Drawing.Point(text3x, gridy);
+            newtextbox2.Name = "newtextbox2";
+            newtextbox2.Size = new System.Drawing.Size(99, 20);
+            newtextbox2.TabIndex = 0;
+            textboxes2.Add(newtextbox2);
+            panel1.Controls.Add(newtextbox2);
+            List<Button> buttons = new List<Button>();
+            Button button = new Button();
+            button.Location = new System.Drawing.Point(text2x, gridy);
+            button.Name = "button"+noofbuttons;
+            button.Size = new System.Drawing.Size(114, 20);
+            button.TabIndex = 0;
+            button.Text = "Browse";
+            button.UseVisualStyleBackColor = true;
+            button.Click += new System.EventHandler(this.button1_Click);
+            buttons.Add(button);
+            panel1.Controls.Add(button);
+
+            gridy += 30;
+
+        }
+
+
+    }
+    }
+
